c55768da57584f7a1a0711473f265742d487df2a
talent-harpreet-singh <harpreet.singh@talentship.io>
2025-07-05 21:26:18 +0530

    Form update to display side by side inouts

--- src/app/components/common/form-field/form-field.component.ts
+++ src/app/components/common/form-field/form-field.component.ts
@@ -10,8 +10,10 @@ import { FormField } from '../../../models/form-model';
   imports: [CommonModule, FormsModule],
   template: `
     <div class="custom-form-group">
-      <label class="custom-form-label">
-        {{ field.label }}:
+      <div class="form-row">
+        <label class="custom-form-label">
+          {{ field.label }}:
+        </label>
         <ng-container [ngSwitch]="field.type">
           <!-- Text Input -->
           <input
@@ -55,14 +57,35 @@ import { FormField } from '../../../models/form-model';
             />
           </div>
         </ng-container>
-      </label>
+      </div>
     </div>
   `,
   styles: [`
+    .custom-form-group {
+      margin-bottom: 1.5rem;
+    }
+    .form-row {
+      display: flex;
+      align-items: center;
+      gap: 10rem;
+    }
+    .custom-form-label {
+      min-width: 180px;
+      font-weight: 600;
+      margin-bottom: 0;
+    }
+    .custom-form-control,
+    .custom-form-select,
+    .tier-profile-input {
+      flex: 1 1 0;
+      min-width: 220px;
+      padding: 0.5rem;
+      font-size: 1rem;
+    }
     .modal-select-container {
       position: relative;
+      width: 100%;
     }
-
     .modal-select-icon {
       position: absolute;
       right: 10px;
