8ca39a062f68127df7bb09730ebcd86f1e6ad9b0
harpreetclouder <harpreet@theai.cloud>
2026-02-04 01:34:17 +0530

    Revert "Added add row and hyperlink"
    This reverts commit 580179a0d3321b23dde6afec3aaa8d781a941475.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
 
@@ -9,7 +8,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
-    provideAnimationsAsync()
+    provideHttpClient(withInterceptorsFromDi())
   ]
 };
--- src/app/models/table.model.ts
+++ src/app/models/table.model.ts
@@ -6,8 +6,6 @@ export interface SimpleTableColumn {
   header: string;
   width?: string;
   sortable?: boolean;
-  clickable?: boolean; // New property to make column clickable
-  clickHandler?: (row: any) => void; // Optional custom click handler
 }
 
 export interface SimpleTableConfig {
